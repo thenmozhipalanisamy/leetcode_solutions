08dca382009a95379d0048632f21f48659670f03
thenmozhipalanisamy <thenmozhipalanisamy2005@gmail.com>
2025-04-11 12:02:43 +0530

    Time: 0 ms (100%), Space: 41 MB (29.94%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,12 +1,5 @@
 class Solution {
     public int hammingWeight(int n) {
-        String binary=Integer.toBinaryString(n);
-       // char[] ch=binary.toCharArray();
-        int[] freq=new int[2];
-
-        for(char ch:binary.toCharArray() ){
-            freq[ch-'0']++;
-        }
-        return freq[1];
+        return Integer.bitCount(n);
     }
 }
